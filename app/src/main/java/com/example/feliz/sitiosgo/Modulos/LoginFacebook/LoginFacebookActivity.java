7fcb7c6ef8fefebb6f78ffe9b006c0fbceda164c
CristianSaldivarLlanos <cristian@bambu-mobile.com>
2018-05-10 15:33:35 -0500

    init ActivityFacebook
    Login con facebook.

---
+++ app/src/main/java/com/example/feliz/sitiosgo/Modulos/LoginFacebook/LoginFacebookActivity.java
@@ -0,0 +1,19 @@
+package com.example.feliz.sitiosgo.Modulos.LoginFacebook;
+
+import android.app.Application;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.feliz.sitiosgo.R;
+
+import java.util.zip.Inflater;
+
+public class LoginFacebookActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginfacebookactivity);
+    }
+}
